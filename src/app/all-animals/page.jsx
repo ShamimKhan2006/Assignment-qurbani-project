f982e063158bac8aa16577bbb8a481f9abb117da
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-05-01 23:29:41 +0600

    login form added

--- src/app/all-animals/page.jsx
+++ src/app/all-animals/page.jsx
@@ -1,27 +1,27 @@
-// import AnimalsCard from '@/components/AnimalsCard';
-// import React from 'react';
+import AnimalsCard from '@/components/AnimalsCard';
+import React from 'react';
 
 
-// const AllAnimalsPage = async() => {
-//     const res=await fetch("http://localhost:3000/data.json")
-//     const animals=await res.json()
-//     console.log(animals)
+const AllAnimalsPage = async() => {
+    const res=await fetch("http://localhost:3000/data.json", {next: { revalidate: 10 }})
+    const animals=await res.json()
+    console.log(animals)
   
-//     return (
-//           <div>
-//              <h1 className='text-center text-white my-10 font-bold text-3xl'>This is Animas details</h1>
-//        <div className='container mx-auto grid grid-cols-1 m-4 md:grid-cols-3 lg:grid-cols-4 gap-4 '>
+    return (
+          <div>
+             <h1 className='text-center text-white my-10 font-bold text-3xl'>This is Animas details</h1>
+       <div className='container mx-auto grid grid-cols-1 m-4 md:grid-cols-3 lg:grid-cols-4 gap-4 '>
               
              
-//             {
-//              animals.map(animal => <AnimalsCard key={animal.id} animal={animal}></AnimalsCard>) 
+            {
+             animals.map(animal => <AnimalsCard key={animal.id} animal={animal}></AnimalsCard>) 
                 
-//             }
+            }
             
-//         </div>
+        </div>
        
-//           </div>
-//     );
-// };
+          </div>
+    );
+};
 
-// export default AllAnimalsPage;
+export default AllAnimalsPage;
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div >
          <Banner></Banner>
-         {/* <AllAnimalsPage></AllAnimalsPage> */}
+         <AllAnimalsPage></AllAnimalsPage>
     </div>
   );
 }
