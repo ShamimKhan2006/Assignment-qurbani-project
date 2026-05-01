419c2c31a6ab59e28c036b5805d840b04b7273c6
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-05-01 23:22:47 +0600

    some bugg

--- src/components/AnimalsCard.jsx
+++ src/components/AnimalsCard.jsx
@@ -1,53 +1,53 @@
-import Image from "next/image";
-import React from "react";
+// import Image from "next/image";
+// import React from "react";
 
-const AnimalsCard = ({ animal }) => {
-  console.log("animal data", animal);
-  const {
-     type,
-     price,
-     name,
-     location,
-    image,
-    description,
-    category,
-     breed,
-     age,
-  } = animal;
-  return (
-    <div className="my-10 ">
+// const AnimalsCard = ({ animal }) => {
+//   console.log("animal data", animal);
+//   const {
+//      type,
+//      price,
+//      name,
+//      location,
+//     image,
+//     description,
+//     category,
+//      breed,
+//      age,
+//   } = animal;
+//   return (
+//     <div className="my-10 ">
      
-       <div className="card bg-[#230D41FF]shadow-2xs text-white border border-black mx-4">
-  <figure>
-    <Image
-      src={image}
-      width={300}
-      height={300}
-      className="object-fill rounded-lg"
-      alt="all animals photo" />
-  </figure>
-  <div className="card-body text-left p-2 ">
-    <h2 className="card-title">
-      {name}
-      <div className="badge badge-secondary">{type}</div>
-    </h2>
-     <div className="flex gap-4">
-         <div>{description}</div>
-        <div>age:{age}</div>
+//        <div className="card bg-[#230D41FF]shadow-2xs text-white border border-black mx-4">
+//   <figure>
+//     <Image
+//       src={image}
+//       width={300}
+//       height={300}
+//       className="object-fill rounded-lg"
+//       alt="all animals photo" />
+//   </figure>
+//   <div className="card-body text-left p-2 ">
+//     <h2 className="card-title">
+//       {name}
+//       <div className="badge badge-secondary">{type}</div>
+//     </h2>
+//      <div className="flex gap-4">
+//          <div>{description}</div>
+//         <div>age:{age}</div>
     
-     </div>
-    <div className="flex gap-4">
-         <p> taka:{price}</p>
-    {location}
-    </div>
-    <div className="card-actions flex gap-4">
-      <div className="badge badge-info">{category}</div>
-      <div className="badge badge-dash">{breed}</div>
-    </div>
-  </div>
-</div>
-    </div>
-  );
-};
+//      </div>
+//     <div className="flex gap-4">
+//          <p> taka:{price}</p>
+//     {location}
+//     </div>
+//     <div className="card-actions flex gap-4">
+//       <div className="badge badge-info">{category}</div>
+//       <div className="badge badge-dash">{breed}</div>
+//     </div>
+//   </div>
+// </div>
+//     </div>
+//   );
+// };
 
-export default AnimalsCard;
+// export default AnimalsCard;
