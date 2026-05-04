255b3a2bbc5930b02a94775872736bb978817352
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-05-04 13:39:09 +0600

    private router aded

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ProfilePage = () => {
+    return (
+        <div className='text-white'>
+            profile page
+        </div>
+    );
+}
+export default ProfilePage;
